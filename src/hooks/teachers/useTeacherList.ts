c7e232d8733185ff6ca6151dfe94bea01f099d60
maxsadbek <masadanb@gmail.com>
2026-03-28 00:31:20 +0500

    add

---
+++ src/hooks/teachers/useTeacherList.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect, useCallback } from "react";
+import { TeacherService } from "@/features/teacher/teacher.service";
+import { Teacher } from "@/features/teacher/teacher.type";
+
+export const useTeacherList = () => {
+	const [data, setData] = useState<Teacher[]>([]);
+	const [totalElements, setTotalElements] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string | null>(null);
+	const [page, setPage] = useState(0);
+	const [size] = useState(10);
+
+	const fetchTeachers = useCallback(async () => {
+		setLoading(true);
+		setError(null);
+		try {
+			const res = await TeacherService.search("", page, size);
+      console.log("data:", res.data);
+			console.log("body:", res.data?.body);
+			console.log("total:", res.data?.totalElements);
+			setData(res.data.body);
+			setTotalElements(res.data.totalElements);
+		} catch (err: any) {
+			setError(err?.response?.data?.message ?? "Xatolik yuz berdi");
+		} finally {
+			setLoading(false);
+		}
+	}, [page, size]);
+
+	useEffect(() => {
+		fetchTeachers();
+	}, [fetchTeachers]);
+
+	return {
+		data,
+		totalElements,
+		loading,
+		error,
+		page,
+		setPage,
+		refetch: fetchTeachers,
+	};
+};
+
